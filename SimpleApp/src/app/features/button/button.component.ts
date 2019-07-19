e3e5fc4495c861a3c185edf9d49a3cba4e8f29df
AndreFelipeFirmo <andreefirmoo@gmail.com>
2019-07-19 17:06:13 -0300

    Finalizando Button

--- SimpleApp/src/app/features/button/button.component.ts
+++ SimpleApp/src/app/features/button/button.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
+
+
 export class ButtonComponent implements OnInit {
 
   constructor() { }
+
+  @Input() classes = 'Primary' || 'Secondary';
+  @Input()
+  value: string;
+  @Input()
+  disabled: boolean;
+  @Input()
+  name = '';
 
   ngOnInit() {
   }
